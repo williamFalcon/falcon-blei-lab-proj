ddfc22bdb845dc750d418bb5dccb43fe714c8f24
William Falcon <waf2107@columbia.edu>
2016-09-25 21:22:08 -0400

    added build script

---
+++ easy_build.sh
@@ -0,0 +1,14 @@
+# clone lda repo
+echo 'loading lda-c library' 
+git clone https://github.com/blei-lab/lda-c.git
+
+#compiling lda
+cd lda-c/
+make
+cd ..
+cp ./lda-c/lda ./app/lda_lib/
+rm -rf lda-c
+echo 'cleaning up. lda bin copied to ./app/lda_lib/'
+
+echo 'starting python application'
+python application.py
